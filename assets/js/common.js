d74895fd69717faf12922bf1e18bb16bc6761b29
takahiro-saeki <hiroooon69@gmail.com>
2016-06-14 14:20:02 +0900

    動く形で

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,13 +1,98 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import STYLE from '../css/style.css';
-import ChatApp from './components/ChatApp.react';
-import ChatExampleData from './ChatExampleData';
-import ChatWebAPIUtils from './utils/ChatWebAPIUtils';
-ChatExampleData.init();
-ChatWebAPIUtils.getAllMessages();
+import {EventEmitter} from 'events';
+import {Dispatcher} from 'flux';
+import assign from 'object-assign';
+
+const testDispatcher = new Dispatcher();
+const CHANGE_EVENT = "change";
+const testConstants = {
+  TEST: "test"
+};
+
+// action
+var TestAction = {
+  test: testValue => {
+    testDispatcher.dispatch({
+      actionType: testConstants.TEST,
+      value: testValue
+    });
+  }
+};
+
+var _test = {value: null};
+var TestStore = assign({}, EventEmitter.prototype, {
+  getAll: () =>  _test,
+  emitChange: function () {
+    this.emit(CHANGE_EVENT);
+  },
+  addChangeListener: function (callback) {
+    this.on(CHANGE_EVENT, callback);
+  },
+  dispatcherIndex: testDispatcher.register(function (payload) {
+    if (payload.actionType === testConstants.TEST) {
+      // console.log(payload.value);
+      _test.value = payload.value;
+      TestStore.emitChange();
+    }
+  })
+});
+
+
+class TestApp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = TestStore.getAll()
+  }
+
+  componentDidMount() {
+    TestStore.addChangeListener(() => this.setState(TestStore.getAll()));
+  }
+
+  render() {
+    return (
+      <div className="testApp">
+        <TestForm />
+        <TestDisplay data={this.state.value} />
+      </div>
+    )
+  }
+}
+
+class TestForm extends Component {
+  constructor(props) {
+    super(props);
+    this.send = this.send.bind(this);
+  }
+
+  send(e) {
+    e.preventDefault();
+    let testValue = ReactDOM.findDOMNode(this.refs.test_value).value.trim();
+    TestAction.test(testValue);
+    ReactDOM.findDOMNode(this.refs.test_value).value = "";
+    return;
+  }
+
+  render() {
+    return (
+      <form>
+        <input type="text" ref="test_value" />
+        <button onClick={this.send}>送信</button>
+      </form>
+    )
+  }
+}
+
+class TestDisplay extends Component {
+  render() {
+    var message = this.props.data;
+    return (
+      <div>{message}</div>
+    )
+  }
+}
 
 ReactDOM.render(
-    <ChatApp />,
-    document.getElementById('app')
+  <TestApp />,
+  document.getElementById("app")
 );
